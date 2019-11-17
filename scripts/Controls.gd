350da0acc76cc9e9d6eced8d7cc54c4067c1787e
NetroScript <NetroScript@users.noreply.github.com>
2019-11-17 17:53:18 +0100

    Added save system

--- scripts/Controls.gd
+++ scripts/Controls.gd
@@ -9,7 +9,7 @@ var music_speed_modifier : float = 1 setget changed_music_speed
 
 var obstacle_amount : int = 8
 
-var locals : String = "en" setget _changed_language
+var locals : String = "de" setget _changed_language
 var languages_available : Array = ["en", "de"]
 
 var speed_modifier : float = 1 setget changed_speed
@@ -19,12 +19,65 @@ var highest_score : int = 0
 
 var screen_modulate : Color = Color(1,1,1) setget changed_modulate
 
+var game_volume : float = 50 setget _change_game_volume
+
+# Settings which will be saved and loaded
+var store_settings : Array = ["locals", "game_volume", "highest_score"]
+var setting_save_file : File
 
 signal speed_change
 signal changed_language
 signal music_speed_change
 signal changed_modulate
 
+
+# Functions to save the current settings
+func save_settings() -> void:
+	# Settings which will be saved
+	var to_save : Dictionary = {}
+	# Iterate list with settings to be saved to store them in the Dictionary
+	for setting in store_settings:
+		to_save[setting] = get(setting)
+
+	# If the setting file doesn't exist create it
+	if setting_save_file == null:
+		setting_save_file = File.new()
+
+	# Open the file and then store settings in it
+	setting_save_file.open("user://settings", File.WRITE)
+	setting_save_file.store_var(to_save)
+	setting_save_file.close()
+
+
+# Load the settings from the settings file
+func load_settings() -> void:
+	# Create file object if it doesn't exist
+	if setting_save_file == null:
+		setting_save_file = File.new()
+
+	# If no settingsfile exists don't load any settings
+	if not setting_save_file.file_exists("user://settings"):
+		return
+
+	# Other wise get the data stored in the file
+	setting_save_file.open("user://settings", File.READ)
+	var data : Dictionary = setting_save_file.get_var()
+	setting_save_file.close()
+
+	# Load every key into the key of this object as long as it is in the settings which should be stored
+	for key in data:
+		if key in store_settings:
+			set(key, data[key])
+
+	# Update the settings menu
+	get_tree().call_group("listens_to_settings_change", "update_settings")
+
+
+# Setter for the game volume
+func _change_game_volume(new_volume : float) -> void:
+	game_volume = new_volume
+	AudioServer.set_bus_volume_db(AudioServer.get_bus_index("Master"), linear2db(new_volume/100))
+	pass
 
 func changed_modulate(new_modulate : Color):
 	
@@ -34,18 +87,19 @@ func changed_modulate(new_modulate : Color):
 
 func reset_values() -> void:
 	
-	speed_modifier = 1
-	music_speed_modifier = 1
-	obstacle_amount = 8
+	self.speed_modifier = 1
+	self.music_speed_modifier = 1
+	self.obstacle_amount = 8
 	
-	pass
+	save_settings()
 
 # Setter for current language, changes the language on change
 func _changed_language(new_language : String) -> void:
-		locals = new_language
-		if TranslationServer.get_locale() != locals:
-			TranslationServer.set_locale(locals)
-			emit_signal("changed_language", locals)
+	locals = new_language
+	if TranslationServer.get_locale() != locals:
+		TranslationServer.set_locale(locals)
+		emit_signal("changed_language", locals)
+	save_settings()
 
 
 func changed_music_speed(new_speed : float):
@@ -63,6 +117,13 @@ func changed_speed(new_speed : float):
 func _ready():
 	
 	randomize()
-	_changed_language("en")
-	changed_speed(speed_modifier)
+	load_settings()
+	reapply()
+	
+	
+	
 	pass
+
+func reapply()->void:
+	_changed_language(locals)
+	_change_game_volume(game_volume)
